1538ebf35d31bb7108223dc15e9423016ce02c91
stutipoudel <stuti.poudel@outlook.com>
2024-01-20 13:17:26 -0500

    code added and updated

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# belly-button-challenge
-Module -14 challenge
+## belly-button-challenge
+## Module -14 challenge
+In this assignment, I build an interactive dashboard to explore the  Belly Button Biodiversity datasetLinks to an external site., which catalogs the microbes that colonize human navels.The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+### Overview on apps.js:
+First I used the D3 library to read in  samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
+Here I have  created an interactive visualization dashboard using D3.js and Plotly where the data selected from the dropdown menu, here 'id' will visualize our data in the form of bar and bubble charts accordingly and also displays the corresponding demographic information in the dashboard. 
+- First I fetched the data from a given URL (a JSON File) using 'd3.json'and initialized the dashboard with init( ) function. 
+- After the data is fetched, it's used to populate a dropdown menu with sample IDs and to initialize visualizations and demographic information for the first sample in the dataset.
+- The script selects a dropdown element in the HTML (#selDataset) and populates it with options. Each option corresponds to a sample ID from the subjectid array, allowing the user to select different samples for visualization.
+- Demographic Information Panel (buildDemographicPanel function) updates a specific HTML element (#sample-metadata) with demographic information of a selected sample. After that we used 'demoinfo.html("")' which clears any existing content in this element and then displays key-value pairs from the sample's metadata under Demographic info panel. 
+- Both functions, 'buildBarchart function' and 'buildBubblechart function' here  process and sort the relevant data from the sample and uses Plotly to render the bar chart and bubble chart respectively.
+- For bar chart, it displays the top 10 OTUs (Operational Taxonomic Units) for the selected sample. Whereas for the bubble chart, it visualizes each OTU as a bubble, with the bubble's size and color representing different data points (they are, otu_ids for color and sample_values for size).
+- I then created 'optionChanged function' , which is triggered when the user selects a different sample ID from the dropdown.
+- It then re-fetches the data and updates the bar chart, bubble chart, and demographic information to reflect the newly selected sample.
+- The code concludes by calling the init function, setting up the initial state of the dashboard when the webpage is loaded.
+
+### Overview on index.html
+This 'index.html' file has a HTML code which is designed for the above "Belly Button Biodiversity Dashboard" which is generated to work in conjunction with the JavaScript code inside 'apps.js' file. 
+- Here, ' <select id="selDataset" onchange="optionChanged(this.value)"></select> ' this html code helps create the dropdown menu  allowing users to select different Test Subject IDs. The onchange attribute is set to call the optionChanged JavaScript function, linking it to the interactive functionality of the dashboard.
+- Likewise, this '<div id="sample-metadata" class="panel-body"></div>' part of html code is also designed for dynamic content updates via JavaScript, which helps in  displaying demographic information related to the selected Test Subject ID in the above generated dashboard.
+- Finally we included the app.js script as a  reference file, which contains the core functionality of the dashboard, such as data fetching, processing, and event handling for the dropdown menu and visualizations.
+
+Reference:
+https://plotly.com/javascript/ (Plotly Documentation)
+
+Thank you !
+
+Stuti Poudel
+
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,156 +1,14 @@
-// // read the url of the JSON Data
-// const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-// function init() {
-
-//     d3.json(url).then(function(data) {
-//         console.log(data);
-//         let subjectid = data.names;
-//         console.log(subjectid);
-    
-//         //Grabbing the reference to the dropdown select menu
-//         let selector = d3.select("#selDataset");
-
-        
-//         // Using the list of sample names to populate the select options
-//         subjectid.forEach((sampledata) => {
-//             selector 
-//             .append("option")
-//             .text(sampledata)
-//             .property("value", sampledata);
-
-//         });
-       
-//          // Initialize the plots with the first sample ID
-//          const firstSampleId = subjectid[0];
-//          buildBarchart(data, firstSampleId);
-//          buildBubblechart(data, firstSampleId);
-//      });
-//  }
-       
-        
-//         // Use the first sample from the list to build the initial plot        
-// function buildBarchart(data) {
-//     let samples = data.samples;
-//     let sample = samples[0];
-
-
-
-//     // Combine the data into a single array of objects
-//     let combinedData = sample.otu_ids.slice(0, 10).map((id, index) => ({
-//     otu_idSample: id,
-//     sample_valueSample: sample.sample_values[index],
-//     otu_labelSample: sample.otu_labels[index]
-
-//  }));
-       
-//     // Sort the combined data by sample_value in descending order
-//     combinedData.sort((a, b) => b.sample_valueSample - a.sample_valueSample);
-    
-//     combinedData.reverse();
-//     console.log(combinedData);
-    
-//         // Create separate arrays for the sorted data
-//         let sortedIds = combinedData.map(d => `OTU ${d.otu_idSample}`);
-//         let sortedValues = combinedData.map(d => d.sample_valueSample);
-//         let sortedLabels = combinedData.map(d => d.otu_labelSample);
-//         console.log(sortedIds);
-//         console.log(sortedValues);
-//         console.log(sortedLabels);
-    
-    
-//         // Create a trace for the bar plot
-//         let trace = {
-//         x: sortedValues,
-//         y: sortedIds,
-//         text: sortedLabels,
-//         type: 'bar',
-//         orientation: 'h'
-//     };
-    
-//         let plotData = [trace];
-    
-//         let layout = {
-//         title: 'Top 10 OTUs',
-//         xaxis: { title: 'Sample Values' },
-//         yaxis: { title: 'OTU IDs' }
-
-//     };
-    
-//         // Render the bar plot
-//         Plotly.newPlot("bar", plotData, layout); 
-
-//         buildBarchart(data);
-
-//         }
-
-//         // Creating a bubble chart displaying each sample
-
-//         function buildBubblechart(data) {
-//             let samplesArray = data.samples;
-//             let sampleFirst = samplesArray[0];
-
-//             // now creating seperate arrays for the data selected
-//             let otu_Ids = sampleFirst.otu_ids;
-//             let sample_values1 = sampleFirst.sample_values;
-//             let otu_labels1 = sampleFirst.otu_labels;
-
-   
-//             let trace1 = {
-//             x: otu_Ids,
-//             y: sample_values1,
-//             mode: 'markers',
-//             marker: {
-//             color: otu_Ids,
-//             size: sample_values1,
-//             sizemode: 'diameter',
-//             colorscale: 'Earth'
-//         },
-//             text: otu_labels1
-//     };
-
-//             let plotData1 = [trace1];
-
-//             let layout1 = {
-//             title: 'Bubble Chart',
-//             xaxis: {title: 'OTU IDs'},
-//             yaxis: {title: 'Sample Values'},
-//             showlegend: false,
-        
-//     };
-
-//     // Render the bubble chart
-//             Plotly.newPlot("bubble", plotData1, layout1);
-
-             
-//         buildBubblechart(data);
-// };
-
-//     // fetching new data each time
-//         function optionChanged(newSampleData) {
-//             buildBarchart(newSampleData);
-//             buildBubblechart(newSampleData);
-
-//         }
-
-
-          
-        
-   
-        
-//     })};
-
-
-
-   
-// init();
-
-
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-
+// Initializing the dashboard with init()
 function init() {
     d3.json(url).then(function(data) {
         let subjectid = data.names;
+        // generating sample metadata, i.e, individual demographic information
+        let metaDataArray = data.metadata;
+        let metadataSample = metaDataArray[0];
+        console.log(metadataSample);
         
+        //populating dropdown selector with sample IDs.
         let selector = d3.select("#selDataset");
         subjectid.forEach((sampledata) => {
             selector.append("option")
@@ -158,10 +16,25 @@ function init() {
                     .property("value", sampledata);
         });
 
+
         // Initialize the plots with the first sample ID
         const firstSampleId = subjectid[0];
         buildBarchart(data, firstSampleId);
         buildBubblechart(data, firstSampleId);
+        buildDemographicPanel(data, firstSampleId);
+    });
+} 
+
+function buildDemographicPanel(data, id) {
+    let demoinfo = d3.select("#sample-metadata");
+    // Clearing existing data
+    demoinfo.html("");  
+
+    let metadataSample = data.metadata.find(sample => sample.id === parseInt(id));
+    console.log(metadataSample);
+    Object.entries(metadataSample).forEach(([key, value]) => {
+        demoinfo.append("p").text(`${key}: ${value}`);
+        console.log(demoinfo);
     });
 }
 
@@ -173,7 +46,7 @@ function buildBarchart(data, id) {
         otu_idSample: id,
         sample_valueSample: sample.sample_values[index],
         otu_labelSample: sample.otu_labels[index]
-    })).sort((a, b) => b.sample_valueSample - a.sample_valueSample);
+    })).sort((a, b) => b.sample_valueSample - a.sample_valueSample).reverse();
 
     // Create separate arrays for sorted data
     let sortedIds = combinedData.map(d => `OTU ${d.otu_idSample}`);
@@ -202,6 +75,7 @@ function buildBarchart(data, id) {
 }
 
 function buildBubblechart(data, id) {
+    console.log(data);
     let sample = data.samples.filter(sample => sample.id === id)[0];
 
     // Create trace for bubble chart
@@ -229,6 +103,7 @@ function buildBubblechart(data, id) {
 
     // Render the bubble chart
     Plotly.newPlot("bubble", plotData1, layout1);
+
 }
 
 function optionChanged(newSampleId) {
@@ -236,6 +111,7 @@ function optionChanged(newSampleId) {
     d3.json(url).then(function(data) {
         buildBarchart(data, newSampleId);
         buildBubblechart(data, newSampleId);
+        buildDemographicPanel(data, newSampleId);
     });
 }
 
